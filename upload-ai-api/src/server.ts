afd61689de707e9d281b026dd25f835c7f3a2a27
Caio Haruo <caio.haruo@hotmail.com>
2023-09-13 10:22:38 -0300

    create upload video route

--- upload-ai-api/src/server.ts
+++ upload-ai-api/src/server.ts
@@ -1,9 +1,19 @@
+import { fastifyMultipart } from '@fastify/multipart';
 import fastify from 'fastify';
+
 import { getAllPromptsRoute } from './routes/get-all-prompts';
+import { uploadVideoRoute } from './routes/upload-video';
 
 const app = fastify();
 
+app.register(fastifyMultipart, {
+  limits: {
+    fileSize: 1_048_576 * 25, // 25mb
+  },
+});
+
 app.register(getAllPromptsRoute);
+app.register(uploadVideoRoute);
 
 app
   .listen({
