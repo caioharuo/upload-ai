3b58a2c282c10698f3464702ed16b7c1b77b44fa
Caio Haruo <caio.haruo@hotmail.com>
2023-09-14 14:37:50 -0300

    Add status message for button

--- upload-ai-web/src/app.tsx
+++ upload-ai-web/src/app.tsx
@@ -49,10 +49,9 @@ export function App() {
           </div>
 
           <p className="text-sm text-muted-foreground">
-            Lembre-se: você pode utilizar a variável {'{ '}
-            <code className="text-violet-400">transcription</code>
-            {' }'} no seu prompt para adicionar o conteúdo da transcrição do
-            vídeo selecionado
+            Lembre-se: você pode utilizar a variável{' '}
+            <code className="text-violet-400">{'{transcription}'}</code> seu
+            prompt para adicionar o conteúdo da transcrição do vídeo selecionado
           </p>
         </div>
 
