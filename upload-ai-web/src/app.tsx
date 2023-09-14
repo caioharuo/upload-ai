b66dddb28c3d5a51b9fe7b154d75a818a6bbadd9
Caio Haruo <caio.haruo@hotmail.com>
2023-09-14 15:49:27 -0300

    final commit

--- upload-ai-web/src/app.tsx
+++ upload-ai-web/src/app.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { Github, Wand2 } from 'lucide-react';
+import { useCompletion } from 'ai/react';
 
 import { Button } from './components/ui/button';
 import { Label } from './components/ui/label';
@@ -20,9 +21,23 @@ export function App() {
   const [temperature, setTemperature] = useState(0.5);
   const [videoId, setVideoId] = useState<string | null>(null);
 
-  function handlePromptSelected(template: string) {
-    console.log(template);
-  }
+  const {
+    input,
+    setInput,
+    handleInputChange,
+    handleSubmit,
+    completion,
+    isLoading,
+  } = useCompletion({
+    api: 'http://localhost:3333/ai/complete',
+    body: {
+      videoId,
+      temperature,
+    },
+    headers: {
+      'Content-type': 'application/json',
+    },
+  });
 
   return (
     <div className="min-h-screen flex flex-col">
@@ -49,11 +64,14 @@ export function App() {
             <Textarea
               className="resize-none p-4 leading-relaxed"
               placeholder="Inclua o prompt para IA..."
+              value={input}
+              onChange={handleInputChange}
             />
             <Textarea
               className="resize-none p-4 leading-relaxed"
               placeholder="Resultado gerado pela IA..."
               readOnly
+              value={completion}
             />
           </div>
 
@@ -69,11 +87,11 @@ export function App() {
 
           <Separator />
 
-          <form className="space-y-6">
+          <form onSubmit={handleSubmit} className="space-y-6">
             <div className="space-y-2">
               <Label>Prompt</Label>
 
-              <PromptSelect onPromptSelected={handlePromptSelected} />
+              <PromptSelect onPromptSelected={setInput} />
             </div>
 
             <div className="space-y-2">
@@ -112,7 +130,7 @@ export function App() {
 
             <Separator />
 
-            <Button type="submit" className="w-full">
+            <Button disabled={isLoading} type="submit" className="w-full">
               Executar
               <Wand2 className="w-4 h-4 ml-2" />
             </Button>
